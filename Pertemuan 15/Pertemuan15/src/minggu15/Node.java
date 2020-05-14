8589c95966e29eed0218f47320eb2f6d613ea934
Dandi29022000 <60720839+Dandi29022000@users.noreply.github.com>
2020-05-14 13:42:32 +0700

    Add files via upload

---
+++ Pertemuan 15/Pertemuan15/src/minggu15/Node.java
@@ -0,0 +1,16 @@
+package minggu15;
+/**
+ *  Nama    : Dandi Agung Setiawan
+ *  NIM     : 1941720009
+ *  Kelas   : TI-1F
+ */
+public class Node {
+
+    int data;
+    Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}

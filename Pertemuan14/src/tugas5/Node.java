baab283e999c7aa407f3535480eb62b703eb7f28
Dandi29022000 <60720839+Dandi29022000@users.noreply.github.com>
2020-05-14 07:37:43 +0700

    Add files via upload

---
+++ Pertemuan14/src/tugas5/Node.java
@@ -0,0 +1,20 @@
+package tugas5;
+
+/**
+ *  Nama    : Dandi Agung Setiawan
+ *  NIM     : 1941720009
+ *  Kelas   : TI-1F
+ */
+public class Node {
+    int data;
+    Node left;
+    Node right;
+    
+    public Node() {
+    }
+    public Node(int data) {
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
